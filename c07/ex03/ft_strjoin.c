d97ebee45de57b33a36b48b53e95ce86fa47406c
“chrisdotswift” <zd30@tutamail.com>
2020-10-06 14:34:47 +0200

    c07/ex03

---
+++ c07/ex03/ft_strjoin.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cduong <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/30 21:22:27 by cduong            #+#    #+#             */
+/*   Updated: 2020/09/30 21:32:36 by cduong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int		ft_strlen(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*len_count(int size, char **strs, char *sep)
+{
+	char	*str;
+	int		len;
+	int		i;
+
+	len = 0;
+	i = 0;
+	while (i < size)
+	{
+		len += ft_strlen(strs[i]);
+		i++;
+	}
+	len += ft_strlen(sep) * size - 1;
+	if (size <= 0)
+		len = 1;
+	if (!(str = (char *)malloc(sizeof(char) * len)))
+		return (0);
+	return (str);
+}
+
+char	*ft_strjoin(int size, char **strs, char *sep)
+{
+	int		i;
+	int		j;
+	int		k;
+	char	*str;
+
+	str = len_count(size, strs, sep);
+	i = -1;
+	k = 0;
+	while (++i < size)
+	{
+		j = 0;
+		while (strs[i][j])
+		{
+			str[k++] = strs[i][j];
+			j++;
+		}
+		j = 0;
+		while (sep[j] && i != size - 1)
+		{
+			str[k++] = sep[j];
+			j++;
+		}
+	}
+	str[k] = '\0';
+	return (str);
+}
